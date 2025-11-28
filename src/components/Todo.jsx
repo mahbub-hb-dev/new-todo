86805b26ba8f245555b69a4e442f1cc419146d2f
Mahbub Hasan <mahbub.hb.dev@gmail.com>
2025-11-28 10:53:54 +0600

    Responsive styles has been added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,10 +7,10 @@ const Todo = (props) => {
     }
 
     return (
-    <article className="flex justify-between items-center bg-blue-600 text-white mb-2 p-3 rounded-md">
+    <article className="flex justify-between items-center bg-blue-600 text-white p-3 rounded-md">
         <div>
-            <h2 className="text-xl font-medium"> {title} </h2>
-            <p> {desc} </p>
+            <h2 className="text-[22px] font-medium pr-1"> {title} </h2>
+            <p className="text-justify text-sm pr-2"> {desc} </p>
         </div>
         <div>
             <button onClick={() => {handleClick(props.id)}} className="bg-red-600 px-3 py-1 rounded-full"> Delete </button>
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,7 +3,7 @@ import Todo from "./Todo"
 const Todos = (props) => {
     // console.log(props)
   return (
-    <section>
+    <section className="grid gap-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {props.todos.map((todo) => (
             <Todo key={todo.id} 
             todo={todo.data} 
